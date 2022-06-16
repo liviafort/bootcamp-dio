4a47b101309fc569b402a1366698c7e092fa6817
liviafort <livia.fortunato@academico.ifpb.edu.br>
2022-06-16 16:39:27 -0300

    Interfaces

--- intro-typescript/index.ts
+++ intro-typescript/index.ts
@@ -1,4 +1,21 @@
-function soma(a: number, b: number){
-  return a + b;
+interface Animal{
+  nome: string;
+  tipo: 'terrestre' | 'aquatico';
+  executarRugido(altura: number): void;
 }
-console.log(soma(4, 5));
+
+interface Felino extends Animal{
+  visaoNoturna: Boolean;
+}
+const animal: Animal = {
+  nome: 'Elefante',
+  tipo: 'terrestre',
+  executarRugido: (altura) => (`${altura}dB`)
+}
+
+const felino: Felino = {
+  nome: 'Leão',
+  tipo: 'terrestre',
+  visaoNoturna: true,
+  executarRugido: (altura) => (`${altura}dB`)
+}

758e6ae0437f2122163ae21674a9c6081654d4a0
liviafort <livia.fortunato@academico.ifpb.edu.br>
2022-06-22 10:15:20 -0300

    Adicionando outras variáveis e Tratando erro de export

--- projeto-typescript/app.js
+++ projeto-typescript/app.js
@@ -1,14 +1,21 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 let button = document.getElementById('button');
 let input1 = document.getElementById('input1');
 let input2 = document.getElementById('input2');
-function somar(numero1, numero2) {
-    return numero1 + numero2;
+function somar(numero1, numero2, frase, devePrintar) {
+    let resultado = numero1 + numero2;
+    if (devePrintar) {
+        console.log(frase + resultado);
+    }
+    return resultado;
 }
+let frase = "O resultado da soma é: ";
+let devePrintar = true;
 if (button) {
     button === null || button === void 0 ? void 0 : button.addEventListener('click', () => {
         if (input1 && input2) {
-            console.log(somar(Number(input1.value), Number(input2.value)));
+            console.log(somar(Number(input1.value), Number(input2.value), frase, devePrintar));
         }
     });
 }
--- projeto-typescript/app.ts
+++ projeto-typescript/app.ts
@@ -1,15 +1,23 @@
-let button = document.getElementById('button')
-let input1 = document.getElementById('input1') as HTMLInputElement
-let input2 = document.getElementById('input2') as HTMLInputElement
+export {};
+let button = document.getElementById('button');
+let input1 = document.getElementById('input1') as HTMLInputElement;
+let input2 = document.getElementById('input2') as HTMLInputElement;
 
-function somar(numero1: number, numero2: number){
-  return numero1 + numero2;
+function somar(numero1: number, numero2: number, frase: string, devePrintar: boolean){
+  let resultado = numero1 + numero2
+  if(devePrintar){
+    console.log(frase + resultado)
+  }
+  return resultado;
 }
+
+let frase = "O resultado da soma é: ";
+let devePrintar = true;
 
 if(button){
   button?.addEventListener('click', () => {
     if(input1 && input2){
-      console.log(somar(Number(input1.value), Number(input2.value)));
+      console.log(somar(Number(input1.value), Number(input2.value), frase, devePrintar));
     }
-  }
-)}
+  })
+}
